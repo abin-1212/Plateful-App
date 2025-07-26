7223d52a48fe85a38c75a7314cd18fad97f6ebc6
Abin <abin@gmail.com>
2025-07-26 16:49:46 +0530

    Fix: Added missing defaultTargetPlatform import

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
 import '../models/user_model.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show kIsWeb, defaultTargetPlatform;
 
 class FirebaseService {
   static final FirebaseService _instance = FirebaseService._internal();
